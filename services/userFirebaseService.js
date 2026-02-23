3a737a0ce2a9cc7ba7d1b1c5f1b5dc4fdcc447d5
priyanka <priyanka23ladhani@gmail.com>
2026-02-23 13:17:50 +0530

    merging

--- services/userFirebaseService.js
+++ services/userFirebaseService.js
@@ -77,7 +77,7 @@ console.log(bal,"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
 console.log(pos,bal)
  const entryPrice=pos.entryPrice;
    const qty=   pos.quantity;
-    const  walletBalance=Number(bal);        // walletBalance = margin blocked for pos position
+    const  walletBalance=Number(bal)+Number(pos.marginUsed);        // walletBalance = margin blocked for pos position
      const leverage= pos.marginMultiplier || 1;
     const positionType=  pos.positionType      
 if (qty <= 0) return 0;
